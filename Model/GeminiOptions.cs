c368363cb2817ff8becb42679afe2b7e8da19731
devsantos16 <devsantos1999@gmail.com>
2025-11-23 15:48:20 -0300

    refactor: removendo valores das propriedades de GeminiOptions

--- Model/GeminiOptions.cs
+++ Model/GeminiOptions.cs
@@ -2,8 +2,8 @@ namespace NotificadorGit.Model
 {
     internal class GeminiOptions
     {
-        public string ApiKey { get; set; } = string.Empty;
-        public string Model { get; set; } = "gemini-2.0-flash";
-        public string BaseUrl { get; set; } = "https://generativelanguage.googleapis.com";
+        public string ApiKey { get; set; }
+        public string Model { get; set; }
+        public string BaseUrl { get; set; }
     }
 }
